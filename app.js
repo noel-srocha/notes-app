2e00d5a06686b382f580a9701b773b4ad8855a35
Manoel de Souza <manoelrocha.neto.93@hotmail.com>
2021-12-23 11:20:30 -0300

    Testing yargs library

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
+const getNotes = require('./utils/getNotes');
+
+// The commands below are configurations of instructions for manipulating notes, using the command  line with assistance from yargs.
+
+// Customize yargs version
+yargs.version('1.0.2');
+
+// Creates a new note
+yargs.command({
+  command: 'add',
+  description: 'Adds note',
+  handler: function () {
+    console.log('Adding the new note!');
+  }
+});
+
+// Removes a note
+yargs.command({
+  command: 'remove',
+  description: 'Removes a note',
+  handler: function () {
+    console.log('Removing the note!');
+  }
+});
+
+// Opens a note
+yargs.command({
+  command: 'open',
+  description: 'Opens a note',
+  handler: function () {
+    console.log('Opening the note...');
+  }
+});
+
+// Lists the notes the user saved
+yargs.command({
+  command: 'list',
+  description: 'Lists notes you saved',
+  handler: function () {
+    console.log('Listing notes...');
+  }
+});
 
 console.log(yargs.argv);
