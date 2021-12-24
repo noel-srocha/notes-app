28d85bd8f60e080ae2dd4b25652c8d6449c7c361
Manoel de Souza <manoelrocha.neto.93@hotmail.com>
2021-12-24 09:09:54 -0300

    Loaded features to app.js and updated package.json

--- app.js
+++ app.js
@@ -1,46 +1,72 @@
-const chalk = require('chalk');
+// const chalk = require('chalk');
 const yargs = require('yargs');
-const getNotes = require('./utils/getNotes');
+const { addNote } = require('./utils/addNotes');
+const removeNote = require('./utils/removeNotes');
+const listNotes = require('./utils/listNotes');
+const openNote = require('./utils/openNote');
 
-// The commands below are configurations of instructions for manipulating notes, using the command  line with assistance from yargs.
+// The commands below are configurations of instructions for manipulating
+// notes, using the command  line with assistance from yargs.
 
 // Customize yargs version
-yargs.version('1.0.2');
+yargs.version('1.1.0');
 
 // Creates a new note
 yargs.command({
   command: 'add',
-  description: 'Adds note',
-  handler: function () {
-    console.log('Adding the new note!');
-  }
+  description: 'Adds a note',
+  builder: {
+    title: {
+      description: 'Note title',
+      alias: 't',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      description: 'Note body',
+      alias: 'b',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    addNote(argv.title, argv.body);
+  },
 });
 
 // Removes a note
 yargs.command({
   command: 'remove',
   description: 'Removes a note',
-  handler: function () {
-    console.log('Removing the note!');
-  }
+  builder: {
+    title: {
+      description: 'Note title',
+      alias: 't',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler(argv) {
+    removeNote(argv.title);
+  },
 });
 
 // Opens a note
 yargs.command({
   command: 'open',
   description: 'Opens a note',
-  handler: function () {
-    console.log('Opening the note...');
-  }
+  handler(argv) {
+    openNote(argv.title);
+  },
 });
 
 // Lists the notes the user saved
 yargs.command({
   command: 'list',
   description: 'Lists notes you saved',
-  handler: function () {
-    console.log('Listing notes...');
-  }
+  handler() {
+    listNotes();
+  },
 });
 
 console.log(yargs.argv);
